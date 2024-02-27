075eca850a910dc7da7181101b1b2f842e9fe3f2
nairisfa <nailakrisnazulfa@gmail.com>
2024-02-27 11:52:24 +0700

    Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
-    grade = models.IntegerField()
+    grade = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
+    )
     description = models.TextField()
 
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+
 
 app_name = 'main'
 
 urlpatterns = [
+    path('logout/', logout_user, name='logout'),
+    path('login/', login_user, name='login'),
+    path('register/', register, name='register'),
     path('', show_main, name="show_main"),
     path('create-item', create_item, name='create_item'),
     path('xml/', show_xml, name='show_xml'),
